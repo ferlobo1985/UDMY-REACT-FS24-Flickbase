c0df6716126deddb26bd43298959cdccb55ec07c
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-11-03 15:28:50 -0300

    03-Connecting to DB
    03-Connecting to DB

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri)
 
 // PARSING
 app.use(bodyParser.json());
