3deebe1ec84d34c29784ae385f961b2bd3c5a846
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-11-08 18:40:44 -0300

    60-Add article Formik
    60-Add article Formik

--- frontend/src/components/dashboard/articles/edit_add/add.jsx
+++ frontend/src/components/dashboard/articles/edit_add/add.jsx
@@ -1,6 +1,25 @@
+import { useFormik, FieldArray, FormikProvider } from 'formik';
+import { formValues, validation } from './validationSchema';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { AdminTitle } from '../../../../utils/tools'
 
 const AddArticle = () => {
+    const articles =  useSelector(state=>state.articles);
+    const dispatch =  useDispatch();
+    let navigate =  useNavigate();
+
+    const formik = useFormik({
+        enableReinitialize: true,
+        initialValues:formValues,
+        validationSchema: validation,
+        onSubmit:(values)=>{
+            console.log(values)
+        }   
+    })
+
+
 
     return(
         <>
