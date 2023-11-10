5f8a6f36a324e201994bf8ee714427d7187fca04
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-11-10 00:32:40 -0300

    79-Home load more
    79-Home load more

--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -1,8 +1,38 @@
+import { useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux'
+import { homeLoadMore } from '../../store/actions/articles'
+
+// mui
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button'
+
+
 
 const Home = () => {
+    const articles = useSelector( state=> state.articles);
+    const dispatch = useDispatch();
+
+
+    useEffect(()=>{
+        if(articles.articles.length <= 0){
+            dispatch(homeLoadMore(articles.homeSort))
+        }
+    },[])
+
+
+    const getNextArticles = () => {
+        let skip = articles.homeSort.skip + articles.homeSort.limit;
+        dispatch(homeLoadMore({...articles.homeSort, skip:skip}))
+    }
+
     return(
         <>
-            Home    
+            <Button 
+                variant='outlined'
+                onClick={getNextArticles}
+            >
+                Load more    
+            </Button>    
         </>
     )
 }
